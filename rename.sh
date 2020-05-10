59ebf1367ecb0afc9dc7e412bb362bbe32538e47
rafanime <rafael.lourenco.nunes@gmail.com>
2020-05-10 11:12:40 +0100

    fixes to renaming script

--- rename.sh
+++ rename.sh
@@ -22,7 +22,7 @@ Options:
 -h, --help                         display this usage message and exit
 -p, --package-name [PACKAGE_NAME]  new package name (i.e. com.example.package)
 -t, --title [TITLE]                new app title (i.e. MyApp)
--o, --old-package-name [OLD_PACKAGE]     old package  name to be changed (i.e. com.example.projectname)
+-o, --old-package-name [OLD_PACKAGE]     old package  name to be changed (i.e. com.app.fitness)
 EOF
 
     exit 1
@@ -31,7 +31,6 @@ EOF
 packagename=""
 title=""
 oldpackage=""
-oldtitle=""
 while [ $# -gt 0 ] ; do
     case "$1" in
     -h|--help)
@@ -65,8 +64,9 @@ fi
 
 if [ -z "$title" ] ; then
     usage "Not enough arguments"
+fi
 
-if [ -z "$oldtitle" ] ; then
+if [ -z "$oldpackage" ] ; then
     usage "Not enough arguments"
 fi
 
@@ -83,9 +83,6 @@ renamefolderstructure() {
   fi
   ORIG_DIR=$DIR
 
-  # mv $DIR/oldpackage $DIR/
-  # rmdir $DIR/com/example
-  # rmdir $DIR/com
   IFS='.' read -ra oldpackages <<< "$oldpackage"
   mv $DIR/${oldpackages[0]}/${oldpackages[1]}/${oldpackages[2]} $DIR/
   rmdir $DIR/${oldpackages[0]}/${oldpackages[1]}
@@ -110,5 +107,5 @@ PACKAGE_DIR=$( renamefolderstructure $PACKAGE_DIR )
 # search and replace in files
 PACKAGE_NAME_ESCAPED="${packagename//./\.}"
 OLD_PACKAGE_NAME_ESCAPED="${oldpackage//./\.}"
-LC_ALL=C find $WORKING_DIR/android -type f -exec sed -i "" "s/$OLD_PACKAGE_NAME_ESCAPED/$PACKAGE_NAME_ESCAPED/g" {} +
-LC_ALL=C find $WORKING_DIR/android -type f -exec sed -i "" "s/$OLD_TITLE/$TITLE_NO_SPACES/g" {} +
+LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$OLD_PACKAGE_NAME_ESCAPED/$PACKAGE_NAME_ESCAPED/g" {} +
+LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$OLD_TITLE/$TITLE_NO_SPACES/g" {} +
